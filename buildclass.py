df706259343a33fdc628f43c133f6a4f8d7cfaa1
ethan002 <ethan.lv@feixitek.com>
2019-10-08 10:20:42 +0800

    Ethan

--- buildclass.py
+++ buildclass.py
@@ -6,7 +6,7 @@ import pexpect
 
 
 class shell_pexpect:
-    def __init__(self,cmd):
+    def __init__(self,cmd,partition='n',select='\n',partition_number='\n',sector='\n',partition_size='\n',write_cmd='w'):
         cmd=self.cmd
         child=pexpect.spawn(cmd)
         data=pexpect.run(cmd)
@@ -21,43 +21,42 @@ class shell_pexpect:
         except pexpect.EOF or pexpect.TIMEOUT:
             return self.data
 
-    def disk_single_partition(self):
-        lis_info=[['Command (m for help)'],['Select(default p)'],['Partition number'],['First sector'],['Last sector'],['Command (m for help)']]
-        lis_info_CN=[['命令(输入m获取帮助)'],['Select(default p)'],[('分区号')],['First sector'],['Last sector'],['命令(输入m获取帮助)']]
-        lis_shell=['n','\n','\n','\n','\n','w']
+    # def disk_single_partition(self):
+    #     lis_info=[['Command (m for help)'],['Select(default p)'],['Partition number'],['First sector'],['Last sector'],['Command (m for help)']]
+    #     lis_info_CN=[['命令(输入m获取帮助)'],['Select(default p)'],[('分区号')],['First sector'],['Last sector'],['命令(输入m获取帮助)']]
+    #     lis_shell=['n','\n','\n','\n','\n','w']
+    #     try:
+    #         for i,j in lis_info,lis_shell:
+    #             if self.child.expect(i)==self.value:
+    #                 self.child.sendline(j)
+    #             else:
+    #                 return 'partition failed'
+    #                 self.child.close()
+    #     except pexpect.EOF or pexpect.TIMEOUT:
+    #         self.child.close()
+    #         return'partition failed'
+    #     return 'partition successful'
+    # def disk_manual_partition(self):
+    #     pass
+
+#返回值--0：進入幫助頁  1：無此文件
+    def enter_command(self):
+        str_command=['Command (m for help)','No such file or directory']
         try:
-            for i,j in lis_info,lis_shell:
-                if self.child.expect(i)==self.value:
-                    self.child.sendline(j)
-                else:
-                    return 'partition failed'
-                    self.child.close()
-        except pexpect.EOF or pexpect.TIMEOUT:
-            self.child.close()
-            return'partition failed'
-        return 'partition successful'
-
-
-
-
-
-    def disk_manual_partition(self):
-        pass
-
-
-    def enter_command(self,next_command):
-        str_command=['Command (m for help)']
-        try:
-            if self.child.expect(str_command)==self.value:
+            i=self.child.expect(str_command)
+            if i==self.value:
                 return '0'
+            elif i==1:
+                return '01'
             else:
                 return '1'
         except pexpect.EOF or pexpect.TIMEOUT:
             return 'partition failed'
 
-#返回值--0:主分區 01：拓展分區 02：免費分區
+#返回值--0:主分區 01：拓展分區 02：免費分區 11：無空閒扇區  12：將主分區拓展爲拓展分區
     def enter_select(self, enter_cmd):
-        str_command = ['Select(default p)','Select(default e)','No free sectors available','All primary partitions are in use']
+        str_command = ['Select(default p)','Select(default e)','No free sectors available',
+                       'All primary partitions are in use','To create more partitions']
         try:
             self.child.sendline(enter_cmd)
             i = self.child.expect(str_command)
@@ -66,44 +65,75 @@ class shell_pexpect:
             elif i == 1:
                 return '01'
             elif i == 2:
-                return 'No free sectors available'
+                return '11'
             elif i == 3:
                 return '02'
+            elif i == 4:
+                return '12'
             else:
                 return '1'
         except pexpect.EOF or pexpect.TIMEOUT:
             return 'partition failed'
 
-    def enter_partition_number(self,next_command):
-        str_command=['Partition number']
+#返回值--0：默認值  01：超出範圍，重新輸入
+    def enter_partition_number(self, enter_cmd):
+        str_command=['Partition number','Value out of range']
         try:
-            if self.child.expect(str_command) == self.value:
-                self.child.sendline(next_command)
+            self.child.sendline(enter_cmd)
+            i = self.child.expect(str_command)
+            if i == self.value:
                 return '0'
+            elif i == 1:
+                return '01'
             else:
                 return '1'
         except pexpect.EOF or pexpect.TIMEOUT:
             return 'partition failed'
 
-    def enter_First_sector(self,next_command):
-        str_command = ['First sector']
+#返回值--0：默認或者輸入值  01：超出範圍重新輸入
+    def enter_First_sector(self,enter_cmd):
+        str_command = ['Value out of range','First sector']
         try:
-            if self.child.expect(str_command) == self.value:
-                self.child.sendline(next_command)
+            self.child.sendline(enter_cmd)
+            i = self.child.expect(str_command)
+            if i == 1:
                 return '0'
+            elif i == 0:
+                return '01'
             else:
                 return '1'
         except pexpect.EOF or pexpect.TIMEOUT:
             return 'partition failed'
 
-    def enter_First_sector(self, next_command):
-        str_command = ['Last sector, +sectors or +size']
+#返回值--0：默認
+    def enter_partition_size(self, enter_cmd):
+        str_command = ['Last sector, +sectors or +size','Value out of range']
         try:
-            if self.child.expect(str_command) == self.value:
-                self.child.sendline(next_command)
+            self.child.sendline(enter_cmd)
+            i = self.child.expect(str_command)
+            if i == self.value:
                 return '0'
+            elif i == 1:
+                return '01'
             else:
                 return '1'
         except pexpect.EOF or pexpect.TIMEOUT:
             return 'partition failed'
+
+    def enter_size(self,enter_cmd):
+        str_command = ['Created a new partition','Last sector, +sectors or +size','Value out of range']
+        try:
+            self.child.sendline(enter_cmd)
+            i = self.child.expect(str_command)
+            if i == self.value:
+                return '0'
+            elif i == 1:
+                return '01'
+            else:
+                return '1'
+        except pexpect.EOF or pexpect.TIMEOUT:
+            return 'partition failed'
+
+    def disk_single_partition(self):
+        self.enter_command()
 
