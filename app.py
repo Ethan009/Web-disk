ef171eadfb3f642bc2f3101fe5f5f7172a86358f
Paul.Wen <Paul.Wen@feixitek.com>
2019-10-31 09:31:34 +0800

    Paul,new

--- app.py
+++ app.py
@@ -250,89 +250,154 @@ def hello_world():
 
 @app.route("/San2", methods=['POST', 'GET'])
 def hello():
-    
+    if request.method == "POST":
+        yjfq = request.values.get('yjfq')  # 一键分区
+        yufenqu = request.values.get('yufenqu')  # 预分区磁盘
+        fengquhao = request.values.get('fengquhao')  # 分区号
+        start = request.values.get('start')  # 开始
+        end = request.values.get('end')  # 结束
+        ygzcpm = request.values.get('ygzcpm')  # 预挂载磁盘
+        wjjlj = request.values.get('wjjlj')  # 文件夹路径输入
+        hidden = request.values.get('hidden')  # 文件类型选择
+        xxx = request.values.get('xxx')  # 当前磁盘分区
+
+        # 格式化文件类型
+        if hidden and xxx:
+            os.popen("mkfs.%s /dev/%s " % (hidden, xxx))
+            print("格式化成功")
+        else:
+            # print("格式化失败")
+            pass
+
+        # 一键分区
+        # print(yjfq)
+        if yjfq:
+            child = pexpect.spawn("sudo fdisk /dev/%s" % (yjfq), timeout=3)
+            child.sendline('n')
+            child.sendline('\n')
+            child.sendline('\n')
+            child.sendline('\n')
+            child.sendline('\n')
+            child.sendline('w')
+            print("Successful to Partition")
+        else:
+            # print("faild to Partition")
+            pass
+
+        # 手动分区
+        if yufenqu and fengquhao and start and end:
+            child = pexpect.spawn("sudo fdisk /dev/%s" % (yufenqu), timeout=3)
+            child.sendline('n')
+            child.sendline('p')
+            child.sendline('%s' % (fengquhao))
+            child.sendline('%s' % (start))
+            child.sendline('%s' % (end))
+            child.sendline('w')
+            print("Successful to Partition")
+        else:
+            # print("faild to Partition")
+            pass
+
+        # 磁盘挂载
+        if ygzcpm and wjjlj:
+            os.system("mkdir %s" % (wjjlj))
+            print("创建文件夹成功")
+            os.system("mount /dev/%s %s" % (ygzcpm, wjjlj))
+            print("挂载成功")
+        else:
+            pass
+
     data_key = dicDI.keys()
     return render_template("San2.html",
                            data_key=data_key,
                            data=dicDI 
             )
 
-    
-def send_message():
-    global message_get_ll
-#     一键分区
-    message_get = request.args['message']
-#     详细分区
-    yufenqu = request.args['yufenqu']
-    fqh = request.args['fqh']
-    start = request.args['start']
-    end = request.args['end']
-#     类型
-    hidden = request.args['hidden']
-    xxx = request.args['xxx']
-#     挂载
-    wjlj = request.args['wjlj']
-    ygzcpm = request.args['ygzcpm']
-    if message_get:
-        child = pexpect.spawn("sudo fdisk /dev/%s" % (yjfq), timeout=3)
-        child.sendline('n')
-        child.sendline('\n')
-        child.sendline('\n')
-        child.sendline('\n')
-        child.sendline('\n')
-        child.sendline('w')
-        index = child.expect("Syncing disks") 
-        if (index == 0):
-            message_get_ll = "执行成功"
-            return message_get_ll
-        else:
-            message_get_ll = "执行失败"
-            return message_get_ll
-    else:
-        pass
-    
-    if yufenqu and fqh and start and end:
-        child = pexpect.spawn("sudo fdisk /dev/%s" % (yufenqu), timeout=3)
-        child.sendline('n')
-        child.sendline('p')
-        child.sendline('%s' % (fqh))
-        child.sendline('%s' % (start))
-        child.sendline('%s' % (end))
-        child.sendline('w')
-        index = child.expect("Syncing disks") 
-        if (index == 0):
-            message_get_ll = "执行成功"
-            return message_get_ll
-        else:
-            message_get_ll = "执行失败"
-            return message_get_ll
-
-    else:
-        pass
-    
-    if xxx and hidden:
-        os.popen("mkfs.%s /dev/%s " % (hidden, xxx))
-        message_get_ll = "执行成功"
-        return message_get_ll
-    else:
-        pass
-    
-    if ygzcpm and wjlj:
-        os.system("mkdir %s" % (wjjlj))
-        os.system("mount /dev/%s %s" % (ygzcpm, wjjlj))
-        message_get_ll = "挂载成功"
-        return message_get_ll
-    else:
-        pass
-
-
-@app.route('/change_to_json', methods=['GET'])
-def change_to_json():
-    global message_get_ll
-    message_json = {
-        "message": message_get_ll
-    }
-    return jsonify(message_json)
+# @app.route("/San2", methods=['POST', 'GET'])
+# def hello():
+#     
+#     data_key = dicDI.keys()
+#     return render_template("San2.html",
+#                            data_key=data_key,
+#                            data=dicDI 
+#             )
+# 
+# @app.route("/send_message", methods=['POST', 'GET'])    
+# def send_message():
+#     global message_get_ll
+# #     一键分区
+#     message_get = request.args['message']
+# #     详细分区
+#     yufenqu = request.args['yufenqu']
+#     fqh = request.args['fqh']
+#     start = request.args['start']
+#     end = request.args['end']
+# #     类型
+#     hidden = request.args['hidden']
+#     xxx = request.args['xxx']
+# #     挂载
+#     wjlj = request.args['wjlj']
+#     ygzcpm = request.args['ygzcpm']
+#     if message_get:
+#         child = pexpect.spawn("sudo fdisk /dev/%s" % (yjfq), timeout=3)
+#         child.sendline('n')
+#         child.sendline('\n')
+#         child.sendline('\n')
+#         child.sendline('\n')
+#         child.sendline('\n')
+#         child.sendline('w')
+#         index = child.expect("Syncing disks") 
+#         if (index == 0):
+#             message_get_ll = "执行成功"
+#             return message_get_ll
+#         else:
+#             message_get_ll = "执行失败"
+#             return message_get_ll
+#     else:
+#         pass
+#     
+#     if yufenqu and fqh and start and end:
+#         child = pexpect.spawn("sudo fdisk /dev/%s" % (yufenqu), timeout=3)
+#         child.sendline('n')
+#         child.sendline('p')
+#         child.sendline('%s' % (fqh))
+#         child.sendline('%s' % (start))
+#         child.sendline('%s' % (end))
+#         child.sendline('w')
+#         index = child.expect("Syncing disks") 
+#         if (index == 0):
+#             message_get_ll = "执行成功"
+#             return message_get_ll
+#         else:
+#             message_get_ll = "执行失败"
+#             return message_get_ll
+# 
+#     else:
+#         pass
+#     
+#     if xxx and hidden:
+#         os.popen("mkfs.%s /dev/%s " % (hidden, xxx))
+#         message_get_ll = "执行成功"
+#         return message_get_ll
+#     else:
+#         pass
+#     
+#     if ygzcpm and wjlj:
+#         os.system("mkdir %s" % (wjjlj))
+#         os.system("mount /dev/%s %s" % (ygzcpm, wjjlj))
+#         message_get_ll = "挂载成功"
+#         return message_get_ll
+#     else:
+#         pass
+# 
+# 
+# @app.route('/change_to_json', methods=['GET'])
+# def change_to_json():
+#     global message_get_ll
+#     message_json = {
+#         "message": message_get_ll
+#     }
+#     return jsonify(message_json)
 
 
 if __name__ == '__main__':
